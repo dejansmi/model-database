79bf7b5d8319a697af6b2d6c3e2b77e762387714
dejansmi <smiljanic_dejan@hotmail.com>
2018-04-07 00:00:42 +0200

    First

---
+++ src/main/java/com/dejans/ModelDatabase/ModelDatabaseApplication.java
@@ -0,0 +1,13 @@
+
+package com.dejans.ModelDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelDatabaseApplication.class, args);
+	}
+}
